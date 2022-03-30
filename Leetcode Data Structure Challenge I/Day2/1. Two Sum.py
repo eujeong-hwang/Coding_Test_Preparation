f6d3e39499dba4b9ee13887f4b3c9b901f5258bb
eujeong-hwang <hj2bf@virginia.edu>
2022-03-30 22:42:18 +0900

    [March 30, 2022] feat: Leetcode Challenge 2

--- Leetcode Data Structure Challenge I/Day2/1. Two Sum.py
+++ Leetcode Data Structure Challenge I/Day2/1. Two Sum.py
@@ -1,16 +1,16 @@
 # https://leetcode.com/problems/two-sum/
 
 # Brute Force 방법 -> 매우..느림..!
-from typing import List
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if target == nums[j] + nums[i]:
-                    return [i,j]    
+# from typing import List
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if target == nums[j] + nums[i]:
+#                     return [i,j]    
 
-wth = Solution()
-print(wth.twoSum([1,3,4,5], 7))
+# wth = Solution()
+# print(wth.twoSum([3,3], 6))
 
 # Using Hash Table
 from typing import List
@@ -18,7 +18,14 @@ class Solution:
     def twoSum(self, nums:List[int], target: int) -> List[int]:
         hash_table = {}
 
-        for i in range
+        for i in range(len(nums)):
+            hash_table[nums[i]] = i
+        for j in range(len(nums)):
+            whereAreYou = target - nums[j]
+            if whereAreYou in hash_table and hash_table[whereAreYou] != j:
+                return [j, hash_table[whereAreYou]]
 
+wth = Solution()
+print(wth.twoSum([1,3,4,5], 7))
 
             
