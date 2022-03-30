ad75c227bc73d27ecb7b9d4f39d4a5804b448b45
eujeong-hwang <hj2bf@virginia.edu>
2022-03-30 21:40:22 +0900

    [March 30, 2022] feat: Leetcode Challenge Day2

---
+++ Leetcode Data Structure Challenge I/Day2/1. Two Sum.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/two-sum/
+
+# Brute Force 방법 -> 매우..느림..!
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == nums[j] + nums[i]:
+                    return [i,j]    
+
+wth = Solution()
+print(wth.twoSum([1,3,4,5], 7))
+
+# Using Hash Table
+from typing import List
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+        hash_table = {}
+
+        for i in range
+
+
+            
