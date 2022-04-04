ade63ad57147d0abf345f804fbb7f3d86a2ec355
eujeong-hwang <hj2bf@virginia.edu>
2022-04-04 22:29:26 +0900

    [April 4, 2022] feat: Leetcode Challenge Day 5

--- Leetcode Data Structure Challenge I/Day5/74. Search a 2D Matrix.py
+++ Leetcode Data Structure Challenge I/Day5/74. Search a 2D Matrix.py
@@ -1,6 +1,22 @@
 # https://leetcode.com/problems/search-a-2d-matrix/
 
+# 풀이
+# https://leetcode.com/problems/search-a-2d-matrix/discuss/896821/Python-Simple-binary-search-explained
 from typing import List
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]: 
+            return False
+        m, n = len(matrix[0]), len(matrix)
+        beg, end = 0, m*n - 1
+        while beg < end:
+            mid = (beg + end)//2
+            if matrix[mid//m][mid%m] < target:
+                beg = mid + 1
+            else:
+                end = mid
+        return matrix[beg//m][beg%m] == target
+
+wth = Solution()
+print("1. ", wth.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+print("2. ", wth.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
