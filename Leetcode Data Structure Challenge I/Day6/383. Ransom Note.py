0189877ddcaaa41339ecbb45ff46a67645e36d6b
eujeong-hwang <hj2bf@virginia.edu>
2022-04-05 22:59:19 +0900

    [April 5, 2022] feat: Leetcode Challenge Day 6

--- Leetcode Data Structure Challenge I/Day6/383. Ransom Note.py
+++ Leetcode Data Structure Challenge I/Day6/383. Ransom Note.py
@@ -1,1 +1,21 @@
 # https://leetcode.com/problems/ransom-note/
+
+# from collections import Counter
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         r = Counter(ransomNote)
+#         m = Counter(magazine)
+        
+#         return r&m == ransomNote
+
+# wth = Solution()
+# print(wth.canConstruct('a', 'b'))
+# --> 이거 안됨. 왜냐하면
+
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         a = set(ransomNote) 
+#         for i in a: 
+#             if magazine.count(i) < ransomNote.count(i): 
+#                 return False 
+#         return True 
