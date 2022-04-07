0707fce4992df6c0fc57c8b58ff4c3def4428e6b
eujeong-hwang <hj2bf@virginia.edu>
2022-04-07 22:32:14 +0900

    [April 7, 2022] feat: Leetcode Challenge Day7

--- Leetcode Data Structure Challenge I/Day7/141. Linked List Cycle.py
+++ Leetcode Data Structure Challenge I/Day7/141. Linked List Cycle.py
@@ -1,1 +1,88 @@
-#
+#https://leetcode.com/problems/linked-list-cycle/
+
+# 1)
+# Using Floyd's Tortoise & Hare Algorithm
+# Definition for singly-linked list.
+# 유튜브 따라했는데.. Time complexity 가 O(n)이긴 하지만.. 그렇게 안 빠른듯
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution:
+#     # def __init__(self):
+#     #     self.head = None
+
+#     def hasCycle(self, head: ListNode) -> bool:
+#         slow = fast = head
+
+#         # while fast and fast.next is not null
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+
+#             if slow == fast:
+#                 return True
+#         return False
+
+# llist = Solution()
+# llist.head = ListNode(3)
+# llist.head.next = ListNode(2)
+# llist.head.next.next = ListNode(0)
+# llist.head.next.next.next = ListNode(-4)
+
+
+# 2)
+# 56ms, 위에 코드의 거의 절반 속도!
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# from typing import Optional
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:        
+#             curr = head
+
+#             while curr:
+#                 if curr.val is None:
+#                     return True
+#                 curr.val = None
+#                 curr = curr.next
+#             return False
+
+
+# 3)
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Hash set
+class Solution:
+    def __init__(self):
+        self.head = None
+        self.next = None
+
+    def hasCycle(self, head: ListNode) -> bool:
+        s=set()
+        while head is not None:
+            if head not in s:
+                s.add(head)
+            else:
+                return True
+            head=head.next
+        return False
+
+# 자꾸 이상하네 ㅠㅠㅋㅋㅋ linked list 프린트 어떻게 하즹......
+# llist = Solution()
+# llist.head = ListNode(1)
+# e2 = ListNode(2)
+# # e3 = ListNode(0)
+# # e4 = ListNode(-4)
+
+# llist.head.next = e2
+# # e2.next = e3
+# # e3.next = e4
+
+# print(llist.hasCycle(llist))
--- Leetcode Data Structure Challenge I/Day7/203. Remove Linked List Elements.py
+++ Leetcode Data Structure Challenge I/Day7/203. Remove Linked List Elements.py
@@ -1,1 +1,21 @@
-# 
+# https://leetcode.com/problems/remove-linked-list-elements/
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# 72 ms
+class Solution:
+    def removeElements(self, head: Optional[ListNode], T: int) -> Optional[ListNode]:
+        dummy = ListNode(-1, head)
+        prev = dummy
+        while head:
+            if head.val != T:
+                prev = head
+            else:
+                prev.next = head.next
+            head = head.next
+        return dummy.next
