b18f6151b7ada1553b4b8d8c983f5dc25b4bf0dc
eujeong-hwang <hj2bf@virginia.edu>
2022-04-09 01:39:42 +0900

    [April 8, 2022] feat: Leetcode Challenge Day8

---
+++ Leetcode Data Structure Challenge I/Day8/206. Reverse Linked Lists.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# 36ms
+# Iterative
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
+
+# 56ms
+# Recursive
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        node = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return node
---
+++ Leetcode Data Structure Challenge I/Day8/83. Remove Duplicates From Sorted List.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next     # skip duplicated node
+            cur = cur.next     # not duplicate of current node, move to next node
+        return head
+
