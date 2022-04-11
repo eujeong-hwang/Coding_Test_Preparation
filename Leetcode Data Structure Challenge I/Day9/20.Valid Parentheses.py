050fa2ff234ba2cb91460c43d8cddf7fa805363f
eujeong-hwang <hj2bf@virginia.edu>
2022-04-11 21:56:23 +0900

    [April 11, 2022] feat: Leetcode Challenge Day9

--- Leetcode Data Structure Challenge I/Day9/20.Valid Parentheses.py
+++ Leetcode Data Structure Challenge I/Day9/20.Valid Parentheses.py
@@ -1,5 +1,38 @@
 # https://leetcode.com/problems/valid-parentheses/
 
+# 20ms (처음에 채점할 떄는 69ms인데 다시 해보니 20ms.. 뭐즤...)
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        d = {')' : '(', '}' : '{', ']' : '['}
+        stack1 = ['N']
+        
+        for i in s:
+            if i in d.keys():
+                if stack1.pop() != d[i]:
+                    return False
+            else:
+                stack1.append(i)
+        
+        return len(stack1) == 1
+
+vp = Solution()
+print(vp.isValid('([{}])'))
+
+
+# 43 ms
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        brackets={'}':'{',')':'(',']':'['}
+        for i in s:
+            if i in brackets.values(): # Opening bracket
+                stack.append(i)
+            else:# Closing bracket
+                if stack and brackets[i]==stack[-1] :  
+                    stack.pop()
+                else: 
+                    return False
+        
+        if stack:
+            return False
+        return True
--- Leetcode Data Structure Challenge I/Day9/232. Implement Queue using Stacks.py
+++ Leetcode Data Structure Challenge I/Day9/232. Implement Queue using Stacks.py
@@ -1,1 +1,37 @@
 # https://leetcode.com/problems/implement-queue-using-stacks/
+
+# 28ms
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+        
+    def pop(self) -> int:
+        # Since in stack we can only pop element from last. 
+        # We first pop all the element from stack1 and append it to stack2. 
+        # After that we pop the last element from stack2
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+        
+    def empty(self) -> bool:
+        return len(self.stack1) == 0 and len(self.stack2) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+param_3 = obj.peek()
+param_3 = obj.pop()
+param_4 = obj.empty()
