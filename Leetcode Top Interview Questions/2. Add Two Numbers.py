1aa3634c492ae3d5f1f6c29a4cf8f0037086944c
eujeong-hwang <hj2bf@virginia.edu>
2022-04-14 01:45:23 +0900

    [April 13, 2022] feat: Leetcode

--- Leetcode Top Interview Questions/2. Add Two Numbers.py
+++ Leetcode Top Interview Questions/2. Add Two Numbers.py
@@ -1,10 +1,46 @@
 # https://leetcode.com/problems/add-two-numbers/
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# 혼자 linked list 개념 안 쓰고 이런거나 하고 있었다;;
+# from typing import Optional
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         final_table = []
+        
+#         for i in range(len(l1)):
+#             for j in range(len(l2)):
+#                 if l1[i] + l2[j] > 9 :
+#                     final_table[i] = 0
+#                     final_table[i+1] +=1
+
+#                 elif l1[i] + l2[j] <= 9:
+#                     final_table[i] = l1[i] + l2[j]
+
+#         return final_table
+
+# two_num = Solution()
+# print(two_num.addTwoNumbers([2,4,3], [5,6,4]))                
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        res = dummy = ListNode()
+        carry = 0
+        while l1 or l2:
+            v1, v2 = 0, 0
+            if l1: v1, l1 = l1.val, l1.next
+            if l2: v2, l2 = l2.val, l2.next
+            
+            val = carry + v1 + v2
+            res.next = ListNode(val%10)
+            res, carry = res.next, val//10
+            
+        if carry:
+            res.next = ListNode(carry)
+            
+        return dummy.next
--- Leetcode Top Interview Questions/3. Longest Substring Without Repeating Characters.py
+++ Leetcode Top Interview Questions/3. Longest Substring Without Repeating Characters.py
@@ -1,5 +1,17 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+# 58ms
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+        used = {}
+        max_length = start = 0
+        for i, c in enumerate(s):
+            if c in used and start <= used[c]:
+                start = used[c] + 1
+            else:
+                max_length = max(max_length, i - start + 1)
+
+            used[c] = i
+
+
+        return max_length
