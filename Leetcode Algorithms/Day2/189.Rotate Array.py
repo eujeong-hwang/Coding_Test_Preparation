149753e5a746bf62325aa2ff081b246e3d7fe811
eujeong-hwang <hj2bf@virginia.edu>
2022-04-18 19:02:24 +0900

    [April 18, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day2/189.Rotate Array.py
+++ Leetcode Algorithms/Day2/189.Rotate Array.py
@@ -1,3 +1,27 @@
 # April 18, 2022
 # Algorithms Daily Challenge - Day2
 # Two Pointers Algorithm
+
+from typing import List
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """         
+        def twopt(arr, i, j):
+            while (i < j):
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return arr
+        
+        if k > len(nums):
+            k %= len(nums)
+            
+        if (k > 0):
+            twopt(nums, 0, len(nums) - 1)  # rotate entire array
+            twopt(nums, 0, k - 1)          # rotate array upto k elements
+            twopt(nums, k, len(nums) - 1)  # rotate array from k to end of array
+
+r = Solution()
+print(r.rotate([1,2,3,4,5,6,7], 3))
