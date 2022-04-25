9e03e094c20add893d3fcf0daf3c991c801cf2ed
eujeong-hwang <hj2bf@virginia.edu>
2022-04-25 21:49:16 +0900

    [April 25, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day5/19. Remove Nth Node From End of List.py
+++ Leetcode Algorithms/Day5/19. Remove Nth Node From End of List.py
@@ -1,1 +1,21 @@
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+# April 25, 2022
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        f= s = head
+        # just want the loop, not the variable
+        for _ in range(n):
+            f = f.next
+        if not f:
+            return head.next
+        while f.next:
+            f = f.next
+            s = s.next
+        s.next = s.next.next
+        return head
--- Leetcode Algorithms/Day5/876. Middle of the Linked List.py
+++ Leetcode Algorithms/Day5/876. Middle of the Linked List.py
@@ -1,1 +1,19 @@
 # https://leetcode.com/problems/middle-of-the-linked-list/
+# April 25, 2022
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Two pointer technique
+# 27ms
+class Solution:
+    def middleNode(self, head:ListNode) -> ListNode:
+        s = f = head
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+
+        return s
