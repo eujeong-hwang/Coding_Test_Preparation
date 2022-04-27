c4acba5e066a6ef79a3d12dba58ca21089c75a76
eujeong-hwang <hj2bf@virginia.edu>
2022-04-28 01:15:28 +0900

    [April 28, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day7/695. Max Area of Island.py
+++ Leetcode Algorithms/Day7/695. Max Area of Island.py
@@ -1,1 +1,18 @@
 # https://leetcode.com/problems/max-area-of-island/
+# April 27, 2022
+
+# 197ms
+# 공부 좀 해야겠따.........dfs, bfs
+from typing import List
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if 0 <= i < m and 0 <= j < n and grid[i][j]:
+                grid[i][j] = 0
+                return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
+            return 0
+
+        areas = [dfs(i, j) for i in range(m) for j in range(n) if grid[i][j]]
+        return max(areas) if areas else 0
--- Leetcode Algorithms/Day7/733. Flood Fill.py
+++ Leetcode Algorithms/Day7/733. Flood Fill.py
@@ -1,1 +1,20 @@
 # https://leetcode.com/problems/flood-fill/
+# April 27, 2022
+
+from typing import List
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        r, c = len(image), len(image[0])
+        color = image[sr][sc]
+        def dfs(i, j):
+            if i < 0 or i>=r or j < 0 or j >= c:
+                return
+            if image[i][j] == newColor or image[i][j] != color:
+                return
+            image[i][j] = newColor
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i,j+1)
+            dfs(i, j-1)
+        dfs(sr, sc)
+        return image
