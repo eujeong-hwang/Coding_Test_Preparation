fe6f86f1d57832de40066f0bbcba945dfdcf05c6
eujeong-hwang <hj2bf@virginia.edu>
2022-04-30 02:52:17 +0900

    [April 29, 2022] feat: Leetcode Algorithms

---
+++ Leetcode Algorithms/Day9 (BFS,DFS)/DFS)/542. 01 Matrix.py
@@ -0,0 +1,31 @@
+# 1
+# https://leetcode.com/problems/01-matrix/
+
+
+# https://leetcode.com/problems/01-matrix/discuss/1369741/C%2B%2BJavaPython-BFS-DP-solutions-with-Picture-Clean-and-Concise-O(1)-Space
+# 그래도 조금 끄적여봣다가 포기...그래도 30분은 끄적여보면서 이것저것 시도..
+# 패턴 조금 안듯...
+# 문제부터 이해하는데 오래 걸렸다...또르르
+from collections import deque
+from typing import List
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        DIR = [0, 1, 0, -1, 0]
+
+        q = deque([])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    q.append((r, c))
+                else:
+                    mat[r][c] = -1  # Marked as not processed yet!
+
+        while q:
+            r, c = q.popleft()
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i + 1]
+                if nr < 0 or nr == m or nc < 0 or nc == n or mat[nr][nc] != -1: continue
+                mat[nr][nc] = mat[r][c] + 1
+                q.append((nr, nc))
+        return mat
