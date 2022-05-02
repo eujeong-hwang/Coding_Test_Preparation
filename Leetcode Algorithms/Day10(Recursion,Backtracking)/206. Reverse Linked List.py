7a8c070b53f91a49e5e7a867dff04256048c7877
eujeong-hwang <hj2bf@virginia.edu>
2022-05-03 03:17:15 +0900

    [May 2, 2022] feat: Leetcode Algorithms

---
+++ Leetcode Algorithms/Day10(Recursion,Backtracking)/206. Reverse Linked List.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         node = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return node
+
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
