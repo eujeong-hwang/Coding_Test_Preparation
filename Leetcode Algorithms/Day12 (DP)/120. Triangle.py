e5412374be4f7758bf892e5e671060d39f029007
eujeong-hwang <hj2bf@virginia.edu>
2022-05-05 01:55:08 +0900

    [May 5, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day12 (DP)/120. Triangle.py
+++ Leetcode Algorithms/Day12 (DP)/120. Triangle.py
@@ -1,2 +1,12 @@
 # 3
 # https://leetcode.com/problems/triangle/
+
+from typing import List
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+        f = [0] * (len(triangle) + 1)
+        for row in triangle[::-1]:
+            for i in range(len(row)):
+                f[i] = row[i] + min(f[i], f[i + 1])
+        return f[0]
--- Leetcode Algorithms/Day12 (DP)/70. Climbing Stairs.py
+++ Leetcode Algorithms/Day12 (DP)/70. Climbing Stairs.py
@@ -1,2 +1,20 @@
 # 1
 # https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        
+        if n == 2:
+            return 2
+        
+        else:
+            prev1 = 1
+            prev2 = 2
+
+            for i in range(3, n+1):
+                cur = prev1 + prev2
+                prev1, prev2 = prev2, cur
+            
+            return cur
