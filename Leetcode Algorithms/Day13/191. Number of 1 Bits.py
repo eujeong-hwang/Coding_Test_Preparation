f5aaf8d1454c4695c6613a584bae4788b83abfe9
eujeong-hwang <hj2bf@virginia.edu>
2022-05-05 21:25:12 +0900

    [May 5, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day13/191. Number of 1 Bits.py
+++ Leetcode Algorithms/Day13/191. Number of 1 Bits.py
@@ -1,2 +1,19 @@
 # 2
 # https://leetcode.com/problems/number-of-1-bits/
+
+# https://leetcode.com/problems/number-of-1-bits/discuss/55106/Python-2-solutions.-One-naive-solution-with-built-in-functions.-One-trick-with-bit-operation
+
+# 35ms
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c = 0
+        while n:
+            n &= n - 1
+            c += 1
+        return c
+
+# using built-in function
+# 60ms
+class Solution:
+    def hammingWeight(self, n: int) -> int:    
+        return bin(n).count('1')
--- Leetcode Algorithms/Day13/231. Power of Two.py
+++ Leetcode Algorithms/Day13/231. Power of Two.py
@@ -1,2 +1,7 @@
 # 1
 # https://leetcode.com/problems/power-of-two/
+
+# 28ms
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n and not (n & n - 1)
