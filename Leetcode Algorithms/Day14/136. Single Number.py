9ec0a325aec5eb87bb13d2cf30ef34f6f5942230
eujeong-hwang <hj2bf@virginia.edu>
2022-05-06 17:02:29 +0900

    [May 6, 2022] feat: Leetcode Algorithms

--- Leetcode Algorithms/Day14/136. Single Number.py
+++ Leetcode Algorithms/Day14/136. Single Number.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/single-number/
+# May 6, 2022
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0)+1
+        for key, val in dic.items():
+            if val == 1:
+                return key
--- Leetcode Algorithms/Day14/190. Reverse Bits.py
+++ Leetcode Algorithms/Day14/190. Reverse Bits.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/reverse-bits/
+# May 6, 2022
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(32):
+            ans = (ans << 1) + (n & 1)
+            n >>= 1
+        return ans
